66d95ccc89f384250132f41b5d978fa012389923
morgan4563 <morganpark4563@gmail.com>
2025-07-23 20:59:29 +0900

    Initial Commit

---
+++ SeSAC7HW19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC7HW19
+//
+//  Created by hyunMac on 7/23/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
