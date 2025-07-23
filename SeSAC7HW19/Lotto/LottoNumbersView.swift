6b5db6e7b5485a07810a735b95cae8704e8111b2
morgan4563 <morganpark4563@gmail.com>
2025-07-23 22:59:49 +0900

    feat: LottoNumberView 기본 형태 생성, 수정 필요

--- SeSAC7HW19/Lotto/LottoNumbersView.swift
+++ SeSAC7HW19/Lotto/LottoNumbersView.swift
@@ -8,13 +8,63 @@
 import UIKit
 
 class LottoNumbersView: UIView {
+    let numberStack = UIStackView()
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    let plusLabel = UILabel()
+    let bonusBall = LottoBallView()
+    let bonusText = UILabel()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureHierachy()
+        configureLayout()
+        configureView()
     }
-    */
+
+    required init?(coder: NSCoder) {
+        fatalError("스토리보드로 사용됨, 에러")
+    }
+
+    func configureRandomNumbers() {
+        var numbers = (1...45).shuffled().prefix(7)
+        let bonusNumber = numbers.removeLast()
+        let lottoNumbers = numbers.sorted()
+
+        lottoNumbers.forEach {
+            let ball = LottoBallView()
+            ball.configureNumber(number: $0)
+            numberStack.addArrangedSubview(ball)
+        }
+        bonusBall.configureNumber(number: bonusNumber)
+    }
+}
+
+extension LottoNumbersView: ViewDesignProtocol {
+    func configureHierachy() {
+        self.addSubview(numberStack)
+
+        numberStack.addArrangedSubview(plusLabel)
+        numberStack.addArrangedSubview(bonusBall)
+        numberStack.addArrangedSubview(bonusText)
+    }
+    
+    func configureLayout() {
+        numberStack.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    func configureView() {
+        numberStack.axis = .horizontal
+        numberStack.spacing = 4
+
+        plusLabel.text = "+"
+        plusLabel.font = .systemFont(ofSize: 18, weight: .bold)
+
+        bonusText.text = "보너스"
+        bonusText.font = .systemFont(ofSize: 13, weight: .semibold)
+        bonusText.textColor = .customGray
+    }
+    
 
 }
